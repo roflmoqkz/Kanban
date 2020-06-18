cb1447c62886ec9ab560a7fe4ffba723e66c4258
roflmoqkz <roflmoqkz@gmail.com>
2020-06-18 13:33:28 +0300

    test

--- Presentation/Login.xaml.cs
+++ Presentation/Login.xaml.cs
@@ -20,7 +20,7 @@ namespace Presentation
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow() //testing a commit so things wont fuck up
         {
             InitializeComponent();
         }
